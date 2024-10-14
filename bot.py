2d2ff2237fbcd6431164dcbc11d8f163e238d90a
Aniket Dey <anike.ydxi@gmail.com>
2024-10-14 20:59:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,11 @@ import logging
 import re
 from urllib.parse import urlparse
 from aiogram import Bot, Dispatcher, executor, types
+import subprocess
+
 pattern = '^https:\/\/www\.beatport\.com\/track\/[\w -]+\/\d+$'
 
-API_TOKEN = '5930894396:AAH2XUH0P3mlVBrl2OIZy-kvqi00H5WPldY'
+API_TOKEN = 'YOUR_BOT_TOKEN'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -18,8 +20,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.answer("🤖 Hey There\!!\, I\'m Beatport Downloader Bot ⚡ Developed by \@piklujazz\n\n🗣️ Supported Command\:\n\/download \<track\_url\>\nTo download track\, 💫☢️", parse_mode=types.ParseMode.MARKDOWN_V2)
-
+    await message.answer("Hi\!\, I\'m Beatport Track Downloader Bot\!\nCreated by \@shahnawazkadari\n\nSupported command\:\n\/download \<track\_url\>\nTo download track\.\n\nEx\:\n\n`\/download https\:\/\/www\.beatport\.com\/track\/take\-me\/17038421`\n\nNote\: I currently support single track downloading\.", parse_mode=types.ParseMode.MARKDOWN_V2)
 
 
 @dp.message_handler(commands=['download'])
@@ -27,13 +28,26 @@ async def download(message: types.Message):
     input_text = message.get_args()
     is_valid = re.match(rf'{pattern}', input_text)
     if is_valid:
-        await message.answer("⚙️ fetching audio & uploading...⚡")
+        await message.answer("Sending audio file..... Be patient.")
         url = urlparse(input_text)
         components = url.path.split('/')
+        
+        # Run the orpheus script to download the track
         os.system(f'python orpheus.py {input_text}')
-        filename = os.listdir(f'downloads/{components[-1]}')
-        file = open(f'downloads/{components[-1]}/{filename[0]}', 'rb')
-        await message.answer_audio(file)
+        
+        # Get the downloaded filename
+        filename = os.listdir(f'downloads/{components[-1]}')[0]
+        filepath = f'downloads/{components[-1]}/{filename}'
+        
+        # Convert the downloaded file to FLAC format using ffmpeg
+        flac_file = f'downloads/{components[-1]}/{os.path.splitext(filename)[0]}.flac'
+        subprocess.run(['ffmpeg', '-i', filepath, flac_file])
+        
+        # Send the FLAC file
+        with open(flac_file, 'rb') as file:
+            await message.answer_audio(file)
+        
+        # Clean up the downloaded files
         shutil.rmtree(f'downloads/{components[-1]}')
     else:
         await message.answer('Invalid track link.\nPlease `enter` valid track link')
