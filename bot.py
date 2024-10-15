7b9632f8c2c4ebe36470ef344a45daaa3c2b5b58
Aniket Dey <anike.ydxi@gmail.com>
2024-10-16 01:11:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ logging.basicConfig(level=logging.INFO)
 # Initialize the Telegram Client
 client = TelegramClient('session_name', api_id, api_hash)
 
+
 @client.on(events.NewMessage(pattern='/start'))
 async def send_welcome(event):
     await event.respond("🤖 Hey There\!!\, I\'m Beatport Downloader Bot ⚡ Developed by \@piklujazz\n\n🗣️ Supported Command\:\n\/download \<track\_url\> or \<album\_url\>\nTo download track or album 💫☢️", parse_mode='md')
@@ -58,7 +59,10 @@ async def download_track(input_text, event):
     file_path = f'downloads/{components[-1]}/{filename[0]}'
     
     # Send the file to the user
-    await client.send_file(event.chat_id, file_path)
+    if os.path.exists(file_path):
+        await client.send_file(event.chat_id, file_path)
+    else:
+        await event.respond(f"Error: File {file_path} does not exist.")
     
     # Cleanup the download folder
     shutil.rmtree(f'downloads/{components[-1]}')
@@ -76,14 +80,32 @@ async def download_album(input_text, event):
     album_id = components[-1]
     album_folder = f'downloads/{album_id}'
     
-    # Check if album folder exists and send each track individually
+    # Check if album folder exists
     if os.path.exists(album_folder):
         track_files = os.listdir(album_folder)
         for track in track_files:
             track_path = os.path.join(album_folder, track)
-            # Send each track one by one
-            await client.send_file(event.chat_id, track_path)
-            await event.respond(f"📤 Sent track: {track}")
+            
+            # Check if the path is a file
+            if os.path.isfile(track_path):
+                try:
+                    await client.send_file(event.chat_id, track_path)
+                    await event.respond(f"📤 Sent track: {track}")
+                except Exception as e:
+                    await event.respond(f"Error sending track {track}: {e}")
+            # If it's a directory, check for media files inside that directory
+            elif os.path.isdir(track_path):
+                media_files = os.listdir(track_path)  # List files inside the directory
+                for media_file in media_files:
+                    media_file_path = os.path.join(track_path, media_file)
+                    if os.path.isfile(media_file_path):
+                        try:
+                            await client.send_file(event.chat_id, media_file_path)
+                            await event.respond(f"📤 Sent track: {media_file} from {track}")
+                        except Exception as e:
+                            await event.respond(f"Error sending track {media_file}: {e}")
+                    else:
+                        await event.respond(f"Error: {media_file_path} is not a valid file.")
     
     # Cleanup the downloaded album folder after sending the files
     shutil.rmtree(album_folder)
